3d011f7003b1b05e496036b4cefab18674d1ec35
tramp <wanggang3@staff.sina.com.cn>
2022-06-08 08:12:31 +0800

    update => add BFPaperButton.xcframework

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BFPaperButton",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(name: "BFPaperButton", targets: ["BFPaperButton"]),
+    ],
+    targets: [
+        .binaryTarget(name: "BFPaperButton", path: "BFPaperButton.xcframework")
+    ]
+)
---
+++ Tests/BFPaperButtonTests/BFPaperButtonTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import BFPaperButton
+
+final class BFPaperButtonTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BFPaperButton().text, "Hello, World!")
+    }
+}
